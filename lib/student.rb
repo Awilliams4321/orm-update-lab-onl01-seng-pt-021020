0cd852b4ba301163be2dc08be2407be1bc5bd0de
Amber Williams <awilliams1234@live.com>
2020-04-16 23:29:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,10 +52,9 @@ class Student
 
   def self.find_by_name(name)
     sql = <<-SQL
-    SELECT * FROM students WHERE name = ? LIMIT 1
+    SELECT name FROM students LIMIT 1
     SQL
     DB[:conn].execute(sql, name)
-    end
   end
 
   def update
